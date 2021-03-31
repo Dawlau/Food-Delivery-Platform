5ea7cac035dddbb41275861d0bdd59d7e4d691bb
Dawlau <blahoviciandrei1@gmail.com>
2021-03-31 18:30:28 +0300

    Implemented a generic Pair class

---
+++ src/ro/localhost/DataStructures/Pair.java
@@ -0,0 +1,28 @@
+package ro.localhost.DataStructures;
+
+public class Pair <T, Q>{
+
+    private T first;
+    private Q second;
+
+    public Pair(T first, Q second){
+        this.setFirst(first);
+        this.setSecond(second);
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public Q getSecond() {
+        return second;
+    }
+
+    public void setSecond(Q second) {
+        this.second = second;
+    }
+}
